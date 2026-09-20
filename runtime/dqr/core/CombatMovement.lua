c6c13eb31d0780793abb0dc8897fb48a56291400
MUshihara <95553535+MUshihara@users.noreply.github.com>
2026-09-20 23:36:43 +0800

    revert: restore V1.4 CombatMovement.lua

--- runtime/dqr/core/CombatMovement.lua
+++ runtime/dqr/core/CombatMovement.lua
@@ -1018,8 +1018,6 @@ Runtime.SamuraiLocalGapCandidate = function(
 
                 if inside == 0
                     and clearanceOkay
-                    and physicalPenalty
-                        < CFG.WAVE_PHYSICAL_HARD_PENALTY
                     and crossing
                         <= CFG.SAMURAI_LOCAL_GAP_ROUTE_MAX
                 then
